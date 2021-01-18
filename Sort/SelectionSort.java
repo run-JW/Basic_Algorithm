26db4783a5a1c98c17a39477c984369d35bd51a2
run-JW <65688438+run-JW@users.noreply.github.com>
2021-01-18 23:37:18 +0900

    Update SelectionSort.java

--- Sort/SelectionSort.java
+++ Sort/SelectionSort.java
@@ -39,7 +39,4 @@ public class SelectionSort {
     public static void swapElements(int[] array, int i, int j) {
         int temp = array[i];
         array[i] = array[j];
-        array[j] = temp;
-    }
-    
-    /*
+        array
